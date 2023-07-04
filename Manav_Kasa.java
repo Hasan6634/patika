9aabc55e91b45d7e440cd79f70d5673ac718ad08
Hasan6634 <138626695+Hasan6634@users.noreply.github.com>
2023-07-05 01:00:01 +0300

    Add files via upload

--- Manav_Kasa.java
+++ Manav_Kasa.java
@@ -11,27 +11,25 @@ public class Manav_Kasa {
         Armut = Armut*2.14;
 
         System.out.print("Aldığınız elma kilosunu giriniz :");
-        Elma *= kg.nextDouble();
+        Elma = kg.nextDouble();
+        Elma = Elma*3.67;
 
         System.out.print("Aldığınız domates kilosunu giriniz :");
-        Domates *= kg.nextDouble();
+        Domates = kg.nextDouble();
+        Domates = Domates*1.11;
 
         System.out.print("Aldığınız muz kilosunu giriniz :");
-        Muz *= kg.nextDouble();
+        Muz = kg.nextDouble();
+        Muz = Muz*0.95;
 
         System.out.print("Aldığınız patlıcan kilosunu giriniz :");
-        Patlican *= kg.nextDouble();
+        Patlican = kg.nextDouble();
+        Patlican = Patlican*5.0;
 
         System.out.println("Armut "+ Armut + " Elma "+Elma+" Domates "+Domates+" Muz "+Muz+" Patlıcan "+Patlican);
 
         toplamTutar = Armut + Elma + Domates + Muz + Patlican;
         System.out.println("Toplam tutar :"+ toplamTutar);
-
-
-
-
-
-
 
     }
 }
