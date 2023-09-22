052c185e07b2f78d93af5f9fa93374affda91172
Hasan6634 <138626695+Hasan6634@users.noreply.github.com>
2023-09-22 15:52:58 +0300

    Add files via upload

---
+++ RecursiveMetoduAsalSayiBulma.java
@@ -0,0 +1,31 @@
+import java.util.Scanner;
+
+public class RecursiveMetoduAsalSayiBulma {
+
+    static boolean asalSayi(int number,int mod) {
+        if (number==1){
+            return false;
+        }
+        if (number == 2) {
+            return true;
+        }
+        int result = number % mod;
+        if (mod==1){
+            return true;
+        }
+        if (result == 0) {
+            return false;
+        }
+        return asalSayi(number,mod-1);
+    }
+
+    public static void main(String[] args) {
+        int number;
+        int counter;
+        Scanner input = new Scanner(System.in);
+        System.out.print("Sayıyı giriniz :");
+        number = input.nextInt();
+        System.out.println(asalSayi(number,number-1)?number+" sayısı asaldır":number+" sayısı asal değildir");
+
+    }
+}
